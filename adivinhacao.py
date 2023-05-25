145382f6bc49494c9f0ef4f24043fddb34f78977
Edson Sequeira <127318228+EdsonBJJ@users.noreply.github.com>
2023-05-25 18:27:53 -0300

    :tada: Jogo de adivinhação

---
+++ adivinhacao.py
@@ -0,0 +1,47 @@
+import random
+
+print("*******************************")
+print("Bem vindo ao jogo de adivinhção")
+print("*******************************")
+
+numero_secreto = random.randrange(1, 101)
+total_de_tentativas = 0
+pontos = 1000
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("defina o nível: "))
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+
+    chute_str = input("Digite um número entre 1 e 100: ")
+    print("você digitou: ", chute_str)
+    chute = int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou e fez {} pontos!".format(pontos))
+        break
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+print("Fim de jogo")
